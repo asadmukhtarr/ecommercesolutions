b3ee3f35f7256e3587a350e7e4d205b549833c49
Ameerullah Mayo <ameerullahmayo1866@gmail.com>
2026-04-17 19:02:31 +0500

    Authentication System

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Livewire\BookCrud;
-use App\Livewire\SchoolCrud;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/books', BookCrud::class);
-Route::get('/schools',SchoolCrud::class);
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
