c9178612a6dabf7ecf72f601dc256c020943b121
Ameerullah Mayo <ameerullahmayo1866@gmail.com>
2026-04-18 15:55:48 +0500

    Admin panel

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Admin Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div class="container mt-5">
+
+    <h1 class="text-primary">Admin Dashboard</h1>
+
+    <div class="row mt-4">
+
+        <div class="col-md-4">
+            <div class="card p-3">
+                <h4>Users</h4>
+                <p>Manage all users</p>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card p-3">
+                <h4>Products</h4>
+                <p>Manage products</p>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="card p-3">
+                <h4>Orders</h4>
+                <p>Manage orders</p>
+            </div>
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,13 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+
+Route::middleware(['auth'])->group(function () {
+
+    Route::get('/admin/dashboard', function () {
+        return view('admin.dashboard');
+    });
+
+});
